7358cd63b01185069b2c2214298e89cb207bc140
Aprimus1 <armentano666@gmail.com>
2017-02-18 15:57:26 +0100

    Create function_timer.py

---
+++ function_timer.py
@@ -0,0 +1,12 @@
+import time
+from functools import wraps
+
+def timethis(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        r = func(*args, **kwargs)
+        end = time.time()
+        print end - start
+        return r
+    return wrapper
