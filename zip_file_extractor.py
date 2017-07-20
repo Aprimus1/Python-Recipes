45476adeeed881ab461dd6d2c6faea9e67f1dcbe
Aprimus1 <armentano666@gmail.com>
2017-07-20 15:43:09 +0200

    Update zip_file_extractor.py

--- zip_file_extractor.py
+++ zip_file_extractor.py
@@ -4,7 +4,10 @@ from bs4 import BeautifulSoup
 
 def zip_file_links_generator(url):
     r = requests.get(url)
-    soup = BeautifulSoup(r.text, 'lxml')
-    for link in soup.find_all('a', href=True):
-        if link['href'].endswith(('.zip', '.ZIP')):
-            yield link['href']
+    if r.ok:
+        soup = BeautifulSoup(r.text, 'lxml')
+        for link in soup.find_all('a', href=True):
+            if link['href'].endswith(('.zip', '.ZIP')):
+                yield link['href']
+    else:
+        return None
